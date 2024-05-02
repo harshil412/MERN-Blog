e7b251fd871298237b134441b5cb6eeec80cbc25
harshil412 <harshilj.patel65@gmail.com>
2024-05-02 14:53:09 +0530

    connect to databse

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.listen(3000, ()=>{
     console.log('Server is listing on 3000!!')
+})
+
+mongoose.connect('mongodb://localhost:27017/Blog')
+.then(()=> {
+    console.log('MongoDb is connected')
+})
+.catch((err)=>{
+    console.log(err)
 })
