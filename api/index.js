3a903f68074fa48b96da3b2d39f411c231288396
harshil412 <harshilj.patel65@gmail.com>
2024-05-04 11:06:30 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 const app = express();
+app.use(express.json())
 
 app.listen(3000, ()=>{
     console.log('Server is listing on 3000!!')
@@ -17,3 +19,4 @@ mongoose.connect('mongodb://localhost:27017/Blog')
 })
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
